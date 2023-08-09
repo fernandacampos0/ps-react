968d3bb40462a08f90baac3daed06bc072ce6a09
caiofcosreal <caiofernandescoswosk@hotmail.com>
2023-08-09 16:53:16 -0300

    :sparkles: feat: modal delete user

--- frontend/src/pages/Private/Users/index.jsx
+++ frontend/src/pages/Private/Users/index.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import {useEffect, useState} from "react";
-import {useSearchParams} from "react-router-dom";
-import styles from './styles.module.css';
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import styles from "./styles.module.css";
 
 import BaseApi from "../../../services/Api";
-import {useStateContext} from "../../../context/ContextProvider";
+import { useStateContext } from "../../../context/ContextProvider";
 
-import {toast} from "react-toastify";
-import {findIndex} from "lodash";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
+import { findIndex } from "lodash";
 import ModalUser from "../../../components/Modals/ModalUser";
 import Pagination from "../../../components/Layout/Pagination";
-import {Spinner} from "react-bootstrap";
+import { Spinner } from "react-bootstrap";
 import TableContainer from "../../../components/Layout/TableContainer";
 
 const INITIAL_DATA = {
@@ -25,7 +26,7 @@ const INITIAL_DATA = {
   from: 1,
   to: 1,
   data: [],
-}
+};
 
 export default function Users() {
   let [searchParams, setSearchParams] = useSearchParams();
@@ -33,99 +34,114 @@ export default function Users() {
   let controller = new AbortController();
 
   const INITIAL_FILTERS = {
-    search: searchParams.get('search') || '',
-  }
+    search: searchParams.get("search") || "",
+  };
 
   const INITIAL_QUERY = {
-    sort: 'id',
-    order: 'desc',
+    sort: "id",
+    order: "desc",
     per_page: 10,
-    page: searchParams.get('page') || 1,
-  }
+    page: searchParams.get("page") || 1,
+  };
 
   const [isLoading, setLoading] = React.useState(true);
   const [isFiltering, setFiltering] = React.useState(true);
   const [isPaginating, setPaginating] = React.useState(true);
 
-  const [query, setQuery] = React.useState({...INITIAL_QUERY, ...INITIAL_FILTERS});
-  const [filters, setFilters] = React.useState({...INITIAL_FILTERS});
-  const [tableData, setTableData] = React.useState({...INITIAL_DATA});
+  const [query, setQuery] = React.useState({
+    ...INITIAL_QUERY,
+    ...INITIAL_FILTERS,
+  });
+  const [filters, setFilters] = React.useState({ ...INITIAL_FILTERS });
+  const [tableData, setTableData] = React.useState({ ...INITIAL_DATA });
 
-  const {setNotification} = useStateContext();
+  const { setNotification } = useStateContext();
 
-
-
-  const requestData = (args ={}) => {
+  const requestData = (args = {}) => {
     setLoading(true);
-    let q = {...query, ...args }
-    BaseApi.get('/users', {
+    let q = { ...query, ...args };
+    BaseApi.get("/users", {
       signal: controller.signal,
       params: {
         ...q,
-        search: q.search !== '' ? q.search : undefined,
-      }
-    }).then(response => {
-      setTableData(response.data);
-      setLoading(false);
-      setPaginating(false);
-      setFiltering(false);
-    }).catch(err => {
-      if (err) {
-        console.log(err);
-        toast.error('Erro ao carregar dados da tabela');
-        setTableData({...INITIAL_DATA});
+        search: q.search !== "" ? q.search : undefined,
+      },
+    })
+      .then((response) => {
+        setTableData(response.data);
         setLoading(false);
         setPaginating(false);
         setFiltering(false);
-      }
-    })
-  }
+      })
+      .catch((err) => {
+        if (err) {
+          console.log(err);
+          toast.error("Erro ao carregar dados da tabela");
+          setTableData({ ...INITIAL_DATA });
+          setLoading(false);
+          setPaginating(false);
+          setFiltering(false);
+        }
+      });
+  };
 
   const setSearch = (args = {}) => {
-    let params = {...args};
-    if (filters.search && filters.search !== '') params.q = filters.search;
+    let params = { ...args };
+    if (filters.search && filters.search !== "") params.q = filters.search;
     setSearchParams(params);
-  }
+  };
 
   const handlePagination = (page) => {
-    setSearch({p: page});
+    setSearch({ p: page });
     setPaginating(true);
-    setQuery({...query, page})
-  }
+    setQuery({ ...query, page });
+  };
 
   const handleFilters = (e) => {
     e.preventDefault();
     setSearch();
     setFiltering(true);
-    setQuery({...query, ...filters, page: 1});
-  }
+    setQuery({ ...query, ...filters, page: 1 });
+  };
 
   const handleCreateUser = (user) => {
     setLoading(true);
-    setFilters({...INITIAL_FILTERS});
-    setQuery({...INITIAL_QUERY});
-  }
+    setFilters({ ...INITIAL_FILTERS });
+    setQuery({ ...INITIAL_QUERY });
+  };
 
   const handleUpdateUser = (user) => {
     let data = [...tableData.data];
-    let toUpdate = findIndex(data, {id: user.id});
+    let toUpdate = findIndex(data, { id: user.id });
     if (toUpdate === -1) return;
-    data[toUpdate] = {...data[toUpdate], ...user};
-    setTableData({...tableData, data: data});
-  }
+    data[toUpdate] = { ...data[toUpdate], ...user };
+    setTableData({ ...tableData, data: data });
+  };
 
   const onDelete = (user) => {
-    if (!window.confirm(`Você realmente deseja excluir ${user.name}?`)) {
-      return
-    }
-    BaseApi.delete(`/users/${user.id}`)
-      .then(() => {
-        setNotification("User was successfully deleted.");
-        requestData();
-      }).catch(() => {
-      setLoading(false);
-    })
-  }
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(({ isConfirmed }) => {
+      if (isConfirmed) {
+        setLoading(true);
+        BaseApi.delete(`/users/${user.id}`)
+          .then(res => {
+            toast.success('User deleted successfully!');
+            requestData();
+          })
+          .catch(err => {
+            Swal.fire('Oops!', err?.data?.errors?.[0] || err?.data?.message || 'Ocorreu um erro ao deletar este usuário.', 'error');
+            setLoading(false);
+          }).finally(() => setLoading(false));
+      }
+    });
+  };
 
   useEffect(() => {
     controller = new AbortController();
@@ -133,7 +149,7 @@ export default function Users() {
     return () => {
       controller.abort();
       setLoading(true);
-    }
+    };
   }, [query]);
 
   return (
